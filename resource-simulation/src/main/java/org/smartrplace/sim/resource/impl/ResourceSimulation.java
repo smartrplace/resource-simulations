838e028cfbb6d84c387968d96f5a7cac60e23c19
cnoelle <christoph.noelle@iee.fraunhofer.de>
2018-05-28 15:53:32 +0200

    simulation time distortion fixed; license header applied

--- resource-simulation/src/main/java/org/smartrplace/sim/resource/impl/ResourceSimulation.java
+++ resource-simulation/src/main/java/org/smartrplace/sim/resource/impl/ResourceSimulation.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2018 Smartrplace UG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.smartrplace.sim.resource.impl;
 
 import java.util.ArrayList;
@@ -107,15 +122,13 @@ class ResourceSimulation implements TimerListener {
 			offsetTime = now - time;
 		}
 		// FIXME v1: looks nice, but an offset arises between different timeseries
-		final long diff = coming == null ? 5000 : (coming.getTimestamp() - time); 
-		// v2: no long-term offset, but the inidividual time series is distorted, at least when running at increased simuation speed
-		/*
+//		final long diff = coming == null ? 5000 : (coming.getTimestamp() - time); 
+		// v2: no long-term offset, but the inidividual time series is distorted, at least when running at increased simuation speed; (but not so bad after all)
 		long diff = coming == null ? 5000 : (coming.getTimestamp() + offsetTime - now);
 		if (diff <= 0) {
 			timerElapsed(timer);
 			return;
 		}
-		*/
 		timer.setTimingInterval(diff);
 		timer.resume();
 	}
